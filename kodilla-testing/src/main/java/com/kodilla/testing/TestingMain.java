851880eb7abea6f4b6b71ea6f0e5200cf73ea830
RafalWKot <rafal.kot.poczta@gmail.com>
2018-05-15 10:40:31 +0200

    Adding test forum username

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,31 +5,6 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUserName();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("Ok");
-        }
-        else {
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator();
-
-        if (calculator.add(1, 2) == 3) {
-            System.out.println("Method add - Ok");
-        }
-        else {
-            System.out.println("Method add - Error");
-        }
-
-        if (calculator.subtract(5, 3) == 2) {
-            System.out.println("Method subtract - Ok");
-        }
-        else {
-            System.out.println("Method subtract - Error");
-        }
     }
 }
