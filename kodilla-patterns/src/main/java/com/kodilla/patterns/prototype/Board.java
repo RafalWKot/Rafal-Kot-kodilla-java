0d78bafe35ce6ae21c0ee5569366b12694f0f35c
RafalWKot <rafal.kot.poczta@gmail.com>
2018-06-28 17:07:03 +0200

    Changed task from patterns-prototype
    Note:

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/Board.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/Board.java
@@ -3,7 +3,7 @@ package com.kodilla.patterns.prototype;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Board extends Prototype{
+public class Board extends Prototype<Board>{
     private String name;
     private Set<TasksList> lists = new HashSet<>();
 
@@ -33,11 +33,11 @@ public class Board extends Prototype{
     }
 
     public Board shallowCopy() throws  CloneNotSupportedException {
-        return (Board)super.clone();
+        return super.clone();
     }
 
     public Board deepCopy() throws CloneNotSupportedException {
-        Board clonedBoard = (Board)super.clone();
+        Board clonedBoard = super.clone();
         clonedBoard.lists = new HashSet<>();
         for(TasksList theList : lists) {
             TasksList clonedList = new TasksList(theList.getName());
--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/Prototype.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/Prototype.java
@@ -2,14 +2,15 @@ package com.kodilla.patterns.prototype;
 
 public class Prototype<T> implements Cloneable {
 
+    @Override
+    public T clone() throws CloneNotSupportedException {
+        return (T)super.clone();
+    }
+
+
 //    @Override
-//    public T clone() throws CloneNotSupportedException {
-//        return (T)super.clone();
+//    protected Object clone() throws CloneNotSupportedException {
+//        return super.clone();
 //    }
 
-
-    @Override
-    protected Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
 }
